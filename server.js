c04d46de6bdfe76a828a3c61218c41b309f9a6b9
木口 <jinkaku8@gmail.com>
2024-06-05 20:19:15 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+// .env ファイルで設定したアプリケーションIDを取得する
+const applicationId = process.env.APPLICATION_ID;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html')); // index.htmlを返す
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
