92ff1b2ef0fca8ade1840cb80efdaee0e2cdfac4
木口 <jinkaku8@gmail.com>
2024-06-06 19:49:29 +0900

    Update scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         }
         const materialsData = await response.json();
 
-        if (materialsData.status === 'OK') { // ここで修正
+        if (materialsData.status === '200') { // ここで修正
             const materialsList = document.getElementById('materials-list');
             materialsData.materials.forEach(material => {
                 const materialElement = document.createElement('div');
