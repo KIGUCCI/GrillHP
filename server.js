64020c892b208bc93b48fdccc3cc322d2502122c
木口 <jinkaku8@gmail.com>
2024-06-16 20:34:17 +0900

    Update server.js

--- server.js
+++ server.js
@@ -13,30 +13,25 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// .env ファイルで設定したアプリケーションIDを取得する
 const applicationId = process.env.APPLICATION_ID;
 
-// 静的ファイルを提供する
 app.use(express.static(path.join(__dirname, 'public')));
 
-// CORSを許可するためのミドルウェア
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
-// 環境変数をクライアントに提供するAPIエンドポイントを作成する
-app.get('/api/environment', (req, res) => {
-    res.json({ applicationId });
-});
-
-// プロキシエンドポイント
 app.get('/proxy', async (req, res) => {
-  const apiUrl = `https://api.make.dmm.com/materials/v1?applicationId=${applicationId}`;
+  const apiUrl = `https://api.make.dmm.com/models/list/v1?applicationId=${applicationId}`;
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
+
+    // デバッグ用のログ出力
+    console.log('API response:', data);
+
     res.json(data);
   } catch (error) {
     console.error('Error:', error);
@@ -44,7 +39,6 @@ app.get('/proxy', async (req, res) => {
   }
 });
 
-// ルートリクエストでindex.htmlを返す
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
@@ -53,3 +47,4 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
